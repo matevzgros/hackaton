75ab1d17d8a05d9d87b4bc4c9fbe331fe7a6464b
Rahul Gupta <rahul.gupta@us.ibm.com>
2018-10-03 18:31:37 +0200

    First commit

---
+++ DB2Connectivity/routes/index.js
@@ -0,0 +1,38 @@
+exports.listSysTables = function(ibmdb,connString) {
+    return function(req, res) {
+
+	   	   
+       ibmdb.open(connString, function(err, conn) {
+			if (err ) {
+			 res.send("error occurred " + err.message);
+			}
+			else {
+				conn.query("SELECT * from DASH13811.NPKD FETCH FIRST 10 ROWS ONLY", function(err, tables, moreResultSets) {
+							
+							
+				if ( !err ) { 
+					res.render('tablelist', {
+						"tablelist" : tables,
+						"tableName" : "10 rows from the GOSALESHR.EMPLOYEE table",
+						"message": "Congratulations. Your connection to dashDB is successful."
+						
+					 });
+
+					
+				} else {
+				   res.send("error occurred " + err.message);
+				}
+
+				/*
+					Close the connection to the database
+					param 1: The callback function to execute on completion of close function.
+				*/
+				conn.close(function(){
+					console.log("Connection Closed");
+					});
+				});
+			}
+		} );
+	   
+	}
+	}
